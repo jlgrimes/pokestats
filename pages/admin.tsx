cb1367ae70e85bdf070ab1892f8da5fbb19fef3c
Jared Grimes <jaredlgrimes@hotmail.com>
2022-12-15 12:05:20 -0500

    init form and start sprite api

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,13 +1,23 @@
-import { getSession } from "next-auth/react";
+import { Flex, FormControl, FormLabel, Input } from '@chakra-ui/react';
+import { getSession } from 'next-auth/react';
 
 export default function Admin() {
   return (
-    <div>admin stuff</div>
-  )
+    <Flex>
+      <FormControl>
+        <FormLabel>Name</FormLabel>
+        <Input />
+      </FormControl>
+      <FormControl>
+        <FormLabel>Deck</FormLabel>
+        <Input />
+      </FormControl>
+    </Flex>
+  );
 }
 
 export async function getServerSideProps(context: any) {
-  const session = await getSession(context)
+  const session = await getSession(context);
 
   if (!session) {
     return {
@@ -15,10 +25,10 @@ export async function getServerSideProps(context: any) {
         destination: '/',
         permanent: false,
       },
-    }
+    };
   }
 
   return {
-    props: { session }
-  }
+    props: { session },
+  };
 }
---
+++ pages/api/sprite.ts
@@ -0,0 +1,24 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  url: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${req.query.name}`);
+    const data = await response.json();
+    const sprite = data.sprites.front_default;
+    if (!sprite) throw('no sprite');
+  
+    res.status(200).json({ url: sprite });
+  } catch(err) {
+    return res.status(500).json({ err });
+  } finally {
+    res.end();
+  }
+}
