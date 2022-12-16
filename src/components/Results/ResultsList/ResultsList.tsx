ec2ecafe94d94a1999e43319d58999d6cd0e83ff
Jared Grimes <jaredlgrimes@hotmail.com>
2022-12-15 20:26:58 -0600

    pretty list

--- src/components/Results/ResultsList/ResultsList.tsx
+++ src/components/Results/ResultsList/ResultsList.tsx
@@ -1,12 +1,48 @@
+import {
+  Editable,
+  Stack,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react';
 import { useQuery } from 'react-query';
 import supabase from '../../../lib/supabase/client';
 
 export default function ResultsList() {
   const tournamentName = 'Toronto 2022';
+  const fetchResults = async () => {
+    const res = await supabase.from('Tournament Results').select('*');
+    return res.data;
+  };
   const { data: results } = useQuery(
     `tournament-results-${tournamentName}`,
-    supabase.from('Tournament Results').select('*')
+    fetchResults
   );
 
-  return <div>{JSON.stringify(results)}</div>;
+  return (
+    <TableContainer>
+      <Table>
+        <Thead>
+          <Tr>
+            <Th>Place</Th>
+            <Th>Name</Th>
+            <Th>Deck</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {results?.map((result, idx) => (
+            <Tr key={idx}>
+              <Td>{result.place}</Td>
+              <Td>{result.player_name}</Td>
+              <Td>{result.deck_archetype}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
 }
