d5086a1adad1f49a8848a4c6bda3f01467c04a24
Jared Grimes <jaredlgrimes@hotmail.com>
2022-12-19 23:15:23 -0500

    log performance and made a few optimizations

--- pages/api/live-results.ts
+++ pages/api/live-results.ts
@@ -11,17 +11,24 @@ const fetchPlayerDecks = async (tournamentId: string) => {
 };
 
 const fetchDeckArchetypes = async () => {
+  const perfStart = performance.now();
+
   const res = await supabase
     .from('Deck Archetypes')
     .select('name,defined_pokemon,identifiable_cards');
+
+  console.log('fetchDeckArchetypes:', (performance.now() - perfStart) / 1000, 'sec');
+
   return res.data;
 };
 
 const fetchPlayerProfiles = async () => {
+  const perfStart = performance.now();
+
   const res = await supabase
     .from('Player Profiles')
     .select('id,name,twitter_profile_url');
-  return await res.data?.reduce((acc, player) => {
+  const profiles = await res.data?.reduce((acc, player) => {
     return {
       ...acc,
       [player.name]: {
@@ -31,13 +38,16 @@ const fetchPlayerProfiles = async () => {
       },
     };
   }, {});
+
+  console.log('fetchPlayerProfiles:', (performance.now() - perfStart) / 1000, 'sec');
+  return profiles;
 };
 
 const uploadMissingPlayerProfiles = async (
-  parsedData: Record<string, any>[]
+  parsedData: Record<string, any>[],
+  playerProfiles: Record<string, string> | undefined
 ) => {
-  const playerProfiles: Record<string, string> | undefined =
-    await fetchPlayerProfiles();
+  const perfStart = performance.now();
 
   if (!parsedData || !playerProfiles) {
     return;
@@ -52,7 +62,12 @@ const uploadMissingPlayerProfiles = async (
     },
     []
   );
-  await supabase.from('Player Profiles').insert(missingPlayerRows);
+
+  if (missingPlayerRows.length > 0) {
+    await supabase.from('Player Profiles').insert(missingPlayerRows);
+  }
+
+  console.log('uploadMissingPlayerProfiles:', (performance.now() - perfStart) / 1000, 'sec');
 };
 
 interface DeckArchetype {
@@ -61,10 +76,15 @@ interface DeckArchetype {
   identifiable_cards: any;
 }
 
-const getPlayerDeckObjects = async (tournamentId: string, deckArchetypes: DeckArchetype[] | null) => {
+const getPlayerDeckObjects = async (
+  tournamentId: string,
+  deckArchetypes: DeckArchetype[] | null
+) => {
+  const perfStart = performance.now();
+
   const playerDecks = await fetchPlayerDecks(tournamentId);
 
-  return playerDecks?.map(({ player_name, deck_archetype }) => {
+  const mappedDecks = playerDecks?.map(({ player_name, deck_archetype }) => {
     const deck: Record<string, any> | undefined = deckArchetypes?.find(
       deck => deck.name === deck_archetype
     );
@@ -73,10 +93,14 @@ const getPlayerDeckObjects = async (tournamentId: string, deckArchetypes: DeckAr
       player_name,
       deck: {
         name: deck_archetype,
-        defined_pokemon: deck?.defined_pokemon
+        defined_pokemon: deck?.defined_pokemon,
       },
     };
   });
+
+  console.log('getPlayerDeckObjects:', (performance.now() - perfStart) / 1000, 'sec');
+
+  return mappedDecks;
 };
 
 interface Player {
@@ -106,30 +130,36 @@ const getPlayerDeck = (
   )?.deck;
   const list = player.decklist;
   let inferredArchetypeFromList;
-  
+
   if (list) {
-    inferredArchetypeFromList = deckArchetypes?.find(({ identifiable_cards }) => {
-      return identifiable_cards?.every((identifiableCard: string) => list.pokemon.some((pokemon: Record<string, any>) => pokemon.name === identifiableCard))
-    });
+    inferredArchetypeFromList = deckArchetypes?.find(
+      ({ identifiable_cards }) => {
+        return identifiable_cards?.every((identifiableCard: string) =>
+          list.pokemon.some(
+            (pokemon: Record<string, any>) => pokemon.name === identifiableCard
+          )
+        );
+      }
+    );
   }
 
   return {
     ...(savedDeckInfo ?? {}),
     ...(list ? { list } : {}),
-    ...(inferredArchetypeFromList ?? {})
+    ...(inferredArchetypeFromList ?? {}),
   };
 };
 
-async function mapResultsArray(
+function mapResultsArray(
   resultsArray: any,
   roundNumber: number,
   playerDeckObjects: PlayerDeckObject[] | undefined,
-  deckArchetypes: DeckArchetype[] | null
-): Promise<string[]> {
-  const playerProfiles: Record<string, string> | undefined =
-    await fetchPlayerProfiles();
+  deckArchetypes: DeckArchetype[] | null,
+  playerProfiles: Record<string, string> | undefined
+): string[] {
+  const perfStart = performance.now();
 
-  return resultsArray.map((player: Player) => ({
+  const mappedArray = resultsArray.map((player: Player) => ({
     name: player.name,
     profile: playerProfiles?.[player.name],
     placing: player.placing,
@@ -138,6 +168,10 @@ async function mapResultsArray(
     day2: player.record.wins * 3 + player.record.ties >= 19,
     deck: getPlayerDeck(playerDeckObjects, player, deckArchetypes),
   }));
+
+  console.log('mapResultsArray:', (performance.now() - perfStart) / 1000, 'sec');
+
+  return mappedArray;
 }
 
 type Data = {
@@ -156,31 +190,51 @@ const getRoundNumber = (firstPlace: Record<string, any>) => {
   return highestRound;
 };
 
+const getPokedata = async (tournamentId: string) => {
+  const perfStart = performance.now();
+
+  const response = await fetch(
+    `https://pokedata.ovh/standings/${tournamentId}/masters/${tournamentId}_Masters.json`
+  );
+  const data = await response.json();
+
+  console.log('getPokedata:', (performance.now() - perfStart) / 1000, 'sec');
+
+  return data;
+};
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
   try {
-    const response = await fetch(
-      `https://pokedata.ovh/standings/${req.query.id}/masters/${req.query.id}_Masters.json`
-    );
-    let parsedData = await response.json();
+    const startTime = performance.now();
+
+    let parsedData = await getPokedata(req.query.id as string);
     const roundNumber = getRoundNumber(parsedData[0]);
 
     const deckArchetypes = await fetchDeckArchetypes();
+
     const playerDeckObjects = await getPlayerDeckObjects(
       req.query.id as string,
       deckArchetypes
     );
 
-    await uploadMissingPlayerProfiles(parsedData);
+    const playerProfiles: Record<string, string> | undefined =
+      await fetchPlayerProfiles();
+
+    await uploadMissingPlayerProfiles(parsedData, playerProfiles);
 
     parsedData = await mapResultsArray(
       parsedData,
       roundNumber,
       playerDeckObjects,
-      deckArchetypes
+      deckArchetypes,
+      playerProfiles
     );
+    const endTime = performance.now();
+
+    console.log('Total time:', (endTime - startTime) / 1000, 'sec');
 
     res.status(200).json({ roundNumber, data: parsedData });
   } catch (err) {
