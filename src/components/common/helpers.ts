e35e485bd4fba56bdb8238ddd2c5860169fc10a5
Jared Grimes <jaredlgrimes@hotmail.com>
2022-12-23 19:28:46 -0500

    adds zero padding to pokedex numbers

--- src/components/common/helpers.ts
+++ src/components/common/helpers.ts
@@ -1,5 +1,5 @@
 export const getSpriteUrl = (pokedexNumber: string) =>
-  `https://www.serebii.net/pokedex-swsh/icon/${pokedexNumber}.png`;
+  `https://www.serebii.net/pokedex-swsh/icon/${pokedexNumber?.padStart(3, '0')}.png`;
 
 export const getHighResSpriteUrl = (pokedexNumber: string) =>
-  `https://www.serebii.net/swordshield/pokemon/${pokedexNumber}.png`;
+  `https://www.serebii.net/swordshield/pokemon/${pokedexNumber?.padStart(3, '0')}.png`;
