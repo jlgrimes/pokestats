f38f38cda496fd58373d360d39841f5669d347d8
Jared Grimes <jaredlgrimes@hotmail.com>
2022-12-28 23:10:49 -0500

    remove edits on profile page for more space

--- src/components/DataDisplay/PlayerPerformanceList.tsx
+++ src/components/DataDisplay/PlayerPerformanceList.tsx
@@ -68,7 +68,7 @@ export const PlayerPerformanceList = ({
                     <DeckInfoDisplay
                       tournament={tournament}
                       player={performance}
-                      enableEdits={userMatchesLoggedInUser}
+                      enableEdits={false}
                     />
                   </Td>
                 </Tr>
