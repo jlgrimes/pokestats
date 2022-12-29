32b4b7bf2ef1c90b9c176ab38a3b7aadddf3758a
Jared Grimes <jaredlgrimes@hotmail.com>
2022-12-29 14:58:55 -0500

    put modal controls on the bottom

--- src/components/Deck/ListViewer/ListViewerModal.tsx
+++ src/components/Deck/ListViewer/ListViewerModal.tsx
@@ -4,10 +4,10 @@ import {
   ModalContent,
   ModalCloseButton,
   ModalBody,
-  ModalHeader,
   Stack,
   Heading,
   Text,
+  CloseButton,
 } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import { Card } from '../../../../types/tournament';
@@ -21,7 +21,7 @@ interface ListViewerModalProps {
   tournamentName: string;
 }
 
-const ListModalBody = ({ list }: {list:  Record<string, Card[]> }) => {
+const ListModalBody = ({ list }: { list: Record<string, Card[]> }) => {
   const [listGridHeight, setListGridHeight] = useState(0);
 
   useEffect(() => {
@@ -42,15 +42,17 @@ export const ListViewerModal = (props: ListViewerModalProps) => {
     <Modal isOpen={props.isOpen} onClose={props.onClose} size='full'>
       <ModalOverlay />
       <ModalContent margin={{ base: '0', sm: '16' }}>
-        <Stack spacing={0} padding={3}>
-          <Heading size='md'>{props.result.deck.name}</Heading>
-          <Text>
-            {props.result.name} - {ordinalSuffixOf(props.result.placing)} @{' '}
-            {props.tournamentName}
-          </Text>
-        </Stack>
-        <ModalCloseButton />
         <ListModalBody list={props.result.deck.list} />
+        <Stack direction='row' justifyContent='space-between' alignItems={'center'}>
+          <Stack spacing={0} padding={3}>
+            <Heading size='md'>{props.result.deck.name}</Heading>
+            <Text>
+              {props.result.name} - {ordinalSuffixOf(props.result.placing)} @{' '}
+              {props.tournamentName}
+            </Text>
+          </Stack>
+          <CloseButton onClick={props.onClose} paddingRight={4} />
+        </Stack>
       </ModalContent>
     </Modal>
   );
