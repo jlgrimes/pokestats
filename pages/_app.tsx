30bd013d507997e47b0030be731a81cdf548b726
Jared Grimes <jaredlgrimes@hotmail.com>
2023-01-01 14:19:39 -0500

    refactored a bit for tests

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,5 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
-import { SessionProvider } from 'next-auth/react';
-import {
-  Hydrate,
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query';
-import { useState } from 'react';
 import { AppLayout } from '../src/components/Layout/AppLayout';
 
 export default function App({
--- src/stories/pages/my-results.stories.tsx
+++ src/stories/pages/my-results.stories.tsx
@@ -1,15 +1,13 @@
-import { QueryClientProvider } from '@tanstack/react-query';
 import MyResults from '../../../pages/tournaments/[id]/my-results';
-import App from '../../../pages/_app';
 import { AppLayout } from '../../components/Layout/AppLayout';
 
 const meta = {
-  title: 'Pages/Home',
+  title: 'Pages/MyResults',
   component: MyResults,
 };
 export default meta;
 
-export const HomePage = () => (
+export const MyResultsPage = () => (
   <AppLayout session={null} dehydratedState={null}>
     <MyResults tournament={{ id: 'mockid', name: 'Mock Regionals 2022' }} />
   </AppLayout>
