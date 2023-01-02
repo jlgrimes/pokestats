7e9492ff86937c3c1606417a84cfdbc956310d16
Jared Grimes <jaredlgrimes@hotmail.com>
2023-01-02 15:56:40 -0500

    readded next config

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['limitlesstcg.s3.us-east-2.amazonaws.com']
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/pokedata/:path*',
+        destination: 'https://pokedata.ovh/:path*',
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
