0b31f962019569cd250873bd9ad2b4221bc5cde3
Jared Grimes <jaredlgrimes@hotmail.com>
2023-01-03 14:43:16 -0500

    vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { AppLayout } from '../src/components/Layout/AppLayout';
 import { SessionProvider } from 'next-auth/react';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({
   Component,
@@ -11,6 +12,7 @@ export default function App({
     <SessionProvider session={session}>
       <AppLayout dehydratedState={pageProps.dehydratedState}>
         <Component {...pageProps} />
+        <Analytics />
       </AppLayout>
     </SessionProvider>
   );
