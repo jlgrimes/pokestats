6ae9fb2457a01354a1b3e090719b009842daff46
Jared Grimes <jaredlgrimes@hotmail.com>
2023-01-03 20:11:58 -0500

    add beta flag

--- src/components/Layout/AppBar.tsx
+++ src/components/Layout/AppBar.tsx
@@ -42,24 +42,27 @@ export const AppBar = () => {
         <Stack direction='row' alignItems='baseline'>
           <LinkBox>
             <LinkOverlay href={`/`}>
-              <Stack direction={'row'} alignItems='center' spacing={0}>
-                <Heading
-                  color={'gray.700'}
-                  letterSpacing={'wider'}
-                  size={'md'}
-                  fontWeight={'black'}
-                  lineHeight={'taller'}
-                >
-                  pokéstats
-                </Heading>
-                <Heading
-                  letterSpacing={'wider'}
-                  size={'md'}
-                  fontWeight={'black'}
-                  color='red.600'
-                >
-                  .live
-                </Heading>
+              <Stack direction={'row'} alignItems='center'>
+                <Stack direction={'row'} alignItems='center' spacing={0}>
+                  <Heading
+                    color={'gray.700'}
+                    letterSpacing={'wider'}
+                    size={'md'}
+                    fontWeight={'black'}
+                    lineHeight={'taller'}
+                  >
+                    pokéstats
+                  </Heading>
+                  <Heading
+                    letterSpacing={'wider'}
+                    size={'md'}
+                    fontWeight={'black'}
+                    color='red.600'
+                  >
+                    .live
+                  </Heading>
+                </Stack>
+                <Badge colorScheme={'yellow'}>beta</Badge>
               </Stack>
             </LinkOverlay>
           </LinkBox>
