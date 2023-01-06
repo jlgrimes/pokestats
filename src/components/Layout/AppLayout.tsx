c26a5f7cdf1719a0b169e526965d93ae523b1dd9
Jared Grimes <jaredlgrimes@hotmail.com>
2023-01-06 00:58:15 -0500

    lets hope it fixes the ugly blue outline

--- src/components/Layout/AppLayout.tsx
+++ src/components/Layout/AppLayout.tsx
@@ -1,4 +1,4 @@
-import { Stack } from '@chakra-ui/react';
+import { extendTheme, Stack } from '@chakra-ui/react';
 import React, { ReactNode } from 'react';
 import { AppBar } from './AppBar/AppBar';
 import { ChakraProvider } from '@chakra-ui/react';
@@ -10,6 +10,10 @@ import {
 } from '@tanstack/react-query';
 import { useState } from 'react';
 
+const theme = extendTheme({
+  components: { Button: { baseStyle: { _focus: { boxShadow: 'none' } } } },
+});
+
 export const AppLayout = ({
   children,
   dehydratedState,
@@ -20,15 +24,15 @@ export const AppLayout = ({
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-      <QueryClientProvider client={queryClient}>
-        <Hydrate state={dehydratedState}>
-          <ChakraProvider>
-            <Stack spacing={1} height='100%'>
-              <AppBar />
-              {children}
-            </Stack>
-          </ChakraProvider>
-        </Hydrate>
-      </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={dehydratedState}>
+        <ChakraProvider theme={theme}>
+          <Stack spacing={1} height='100%'>
+            <AppBar />
+            {children}
+          </Stack>
+        </ChakraProvider>
+      </Hydrate>
+    </QueryClientProvider>
   );
 };
