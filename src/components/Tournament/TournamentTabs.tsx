89c5cac4cb9043c4c8758d557148b20f369a0a9a
Jared Grimes <jaredlgrimes@hotmail.com>
2023-01-05 23:23:57 -0500

    made my results page load for tabs.. not on that

--- src/components/Tournament/TournamentTabs.tsx
+++ src/components/Tournament/TournamentTabs.tsx
@@ -1,17 +1,25 @@
 import { Tabs, TabList, Tab, Badge, Stack, Text } from '@chakra-ui/react';
+import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import { Tournament } from '../../../types/tournament';
+import { useSessionUserProfile } from '../../hooks/user';
 import { getStandingsBadgeProps } from './helpers';
 
 export const TournamentTabs = ({ tournament }: { tournament: Tournament }) => {
   const router = useRouter();
+  const { data: userProfile } = useSessionUserProfile();
+  const userIsInTournament = userProfile?.tournamentHistory?.includes(
+    tournament.id as string
+  );
+
+
   const tabs = [
     {
       name: 'Standings',
       slug: 'standings',
       badge: getStandingsBadgeProps(tournament),
     },
-    ...(router.asPath.includes('my-results')
+    ...(router.asPath.includes('my-results') || userIsInTournament
       ? [
           {
             name: 'My results',
