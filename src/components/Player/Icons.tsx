6fd7065130fc2ab8eec2afefc19c017ccd917ac4
Jared Grimes <jaredlgrimes@hotmail.com>
2023-01-05 23:43:26 -0500

    changed icon to light blue

--- src/components/Player/Icons.tsx
+++ src/components/Player/Icons.tsx
@@ -2,7 +2,7 @@ import { Icon } from '@chakra-ui/react';
 import { FaCheckCircle, FaTimesCircle } from 'react-icons/fa';
 
 export const VerifiedIcon = () => (
-  <Icon as={FaCheckCircle} color='red.600' zIndex={3} />
+  <Icon as={FaCheckCircle} color='blue.300' zIndex={3} />
 );
 export const NotVerifiedIcon = () => (
   <Icon as={FaTimesCircle} color='gray.500' zIndex={3} />
