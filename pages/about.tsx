a9ebdd56b983581de1d32a9753290dc0c75511b9
Jared Grimes <jaredlgrimes@hotmail.com>
2023-01-10 12:48:40 -0500

    polished up about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,38 +1,69 @@
 import { Flex, Heading, Link, Stack, Text } from '@chakra-ui/react';
 import NextLink from 'next/link';
+import { useMemo } from 'react';
 import { AppLogo } from '../src/components/Layout/AppBar/AppLogo';
 import { useTwitterLink } from '../src/hooks/twitter';
 
 export default function AboutPage() {
   const myTwitter = useTwitterLink('jgrimesey');
+  const tateTwitter = useTwitterLink('twhitesell42');
+  const kashTwitter = useTwitterLink('KashMann27');
   const juTwitter = useTwitterLink('_JuHlien_');
   const simeyTwitter = useTwitterLink('simeydotme');
   const statsTwitter = useTwitterLink('pokestatstcg');
+
+  const RK9Link = () => (
+    <Link isExternal href={'https://rk9.gg/'} as={NextLink} color='blue.500'>
+      RK9.gg
+    </Link>
+  );
 
   return (
     <Stack padding='1.5rem' spacing={12}>
       <Stack spacing={6}>
         <Stack>
-          <Heading size='md'>What is Pokestats Live?</Heading>
+          <Heading size='md'>What is PokéStats Live?</Heading>
           <Text>
-            Pokestats Live is a live tournament tracking tool. Deck archetypes
+            PokéStats Live is a live tournament tracking tool. Deck archetypes
             can be reported in real time during tournaments, allowing for
-            comprehensive metagame analysis as soon as after Round 9.
-          </Text>
-        </Stack>
-        <Stack>
-          <Heading size='md'>How are decks reported?</Heading>
-          <Text>
-            You can report your own deck and opponents you play against if you
-            have an account. Deck reports are reviewed by the Pokestats team and
-            are surfaced onto the site once deemed accurate. The Pokestats team
-            can also submit decks for anyone at the tournament.
+            comprehensive metagame analysis as soon Day 1.
           </Text>
         </Stack>
         <Stack>
           <Heading size='md'>Where is the data coming from?</Heading>
           <Text>
-            Tournament data comes directly from RK9 through{' '}
+            {`Player deck data by default originates from reported decks. Any player's deck archetype for any tournament can be submitted by the PokéStats team, which currently consists of `}
+            <Link isExternal href={myTwitter} as={NextLink} color='twitter.500'>
+              Jared Grimes
+            </Link>
+            ,{' '}
+            <Link
+              isExternal
+              href={tateTwitter}
+              as={NextLink}
+              color='twitter.500'
+            >
+              Tate Whitesell
+            </Link>
+            , and{' '}
+            <Link
+              isExternal
+              href={kashTwitter}
+              as={NextLink}
+              color='twitter.500'
+            >
+              Kashvinder Singh Mann
+            </Link>
+            .{' '}
+            {`Players who are authenticated through logging into PokéStats Live
+            can submit deck archetypes for tournaments they are attending.
+            Attendance is determined by the player's name appearing in the standings list
+            for that tournament at `}
+            <RK9Link />
+          </Text>
+          <Text>
+            Tournament data such as name, date, standings, and round pairings
+            are from <RK9Link />. This data is retrieved through{' '}
             <Link
               isExternal
               href={'https://pokedata.ovh/'}
@@ -41,6 +72,18 @@ export default function AboutPage() {
             >
               pokedata.ovh
             </Link>
+            .
+          </Text>
+        </Stack>
+        <Stack>
+          <Heading size='md'>What if someone reports the wrong deck?</Heading>
+          <Text>
+            Ultimately, this is going to be based off the honor system. Even the
+            PokéStats team can accidentally misreport a deck. All information
+            will be checked to the best of our ability. Procedures have yet to
+            be set in stone for if anyone is intentionally spreading
+            misinformation about deck archetypes repeatedly, but rest assured
+            there will be some procedure.
           </Text>
         </Stack>
         <Stack>
@@ -58,6 +101,8 @@ export default function AboutPage() {
           <Heading size='md'>Credits</Heading>
           <Text>
             {`This project wouldn't be possible without `}
+            <RK9Link />
+            {` for tournament, standings, and match data, `}
             <Link isExternal href={juTwitter} as={NextLink} color='twitter.500'>
               @_JuHlien_
             </Link>
