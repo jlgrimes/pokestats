87b486baf15571b2701182689f9dacfbf940951c
Jared Grimes <jaredlgrimes@hotmail.com>
2023-01-12 21:30:06 -0500

    fixed object entries error

--- src/components/Tournament/Results/Filters/FilterTags.tsx
+++ src/components/Tournament/Results/Filters/FilterTags.tsx
@@ -3,6 +3,11 @@ import { useArchetypes } from '../../../../hooks/deckArchetypes';
 import SpriteDisplay from '../../../common/SpriteDisplay';
 import { StandingsFilters } from './StandingsFilterMenu';
 
+// Needed for Object.Entries. Love typescript.
+type Entries<T> = {
+  [K in keyof T]: [K, T[K]];
+}[keyof T][];
+
 export const FilterTags = ({
   filters,
   toggleFilter,
@@ -14,36 +19,38 @@ export const FilterTags = ({
 
   return (
     <HStack>
-      {Object.entries(filters).map(([key, val], idx) => {
-        if (key === 'decksVisible') {
+      {(Object.entries(filters) as Entries<StandingsFilters>).map(
+        ([key, val], idx) => {
+          if (key === 'decksVisible') {
+            return (
+              <>
+                {val.map((deckId: number, idx: number) => (
+                  <Tag key={idx} borderRadius='full'>
+                    <SpriteDisplay
+                      key={`filter-deck-${idx}`}
+                      pokemonNames={
+                        archetypes?.find(({ id }) => deckId === id)
+                          ?.defined_pokemon
+                      }
+                      squishWidth
+                    />
+                    <TagCloseButton
+                      onClick={() => toggleFilter('decksVisible', deckId)}
+                    />
+                  </Tag>
+                ))}
+              </>
+            );
+          }
+
           return (
-            <>
-              {val.map((deckId: string, idx: number) => (
-                <Tag key={idx} borderRadius='full'>
-                  <SpriteDisplay
-                    key={`filter-deck-${idx}`}
-                    pokemonNames={
-                      archetypes?.find(({ id }) => deckId === id)
-                        ?.defined_pokemon
-                    }
-                    squishWidth
-                  />
-                  <TagCloseButton
-                    onClick={() => toggleFilter('decksVisible', deckId)}
-                  />
-                </Tag>
-              ))}
-            </>
+            <Tag key={idx} borderRadius='full'>
+              <TagLabel>{val.name}</TagLabel>
+              <TagCloseButton onClick={() => toggleFilter(key)} />
+            </Tag>
           );
         }
-
-        return (
-          <Tag key={idx} borderRadius='full'>
-            <TagLabel>{val.name}</TagLabel>
-            <TagCloseButton onClick={() => toggleFilter(key)} />
-          </Tag>
-        );
-      })}
+      )}
     </HStack>
   );
 };
