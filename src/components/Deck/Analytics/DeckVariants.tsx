31c5297fa6a5b9958549da8f732bf815fc1e8093
Jared Grimes <jaredlgrimes@hotmail.com>
2023-01-14 18:25:39 -0500

    hide variant text if there's no variants

--- src/components/Deck/Analytics/DeckVariants.tsx
+++ src/components/Deck/Analytics/DeckVariants.tsx
@@ -15,34 +15,34 @@ import SpriteDisplay from '../../common/SpriteDisplay';
 
 export const DeckVariants = memo(({ deck }: { deck: Deck }) => {
   const { data: variants } = useVariants(deck.supertype);
-  console.log(variants);
+  const variantsExcludingSelf = variants?.filter(({ id }) => id !== deck.id);
+
+  if (!(variantsExcludingSelf && variantsExcludingSelf.length > 0)) return null;
 
   return (
     <HStack flexWrap={'wrap'} rowGap={1}>
       <Heading size='sm' color='gray.600'>
         {deck.supertype} Variants:
       </Heading>
-      {variants
-        ?.filter(({ id }) => id !== deck.id)
-        .map(variant => (
-          <LinkBox key={`variant-${variant.name}`}>
-            <LinkOverlay as={NextLink} href={`/decks/${variant.id}`}>
-              <Tag>
-                <HStack spacing={1}>
-                  <SpriteDisplay
-                    squishWidth
-                    pokemonNames={
-                      variant.defined_pokemon.length > 1 &&
-                      variant.defined_pokemon[1].length > 1
-                        ? [variant.defined_pokemon[1]]
-                        : [variant.defined_pokemon[0]]
-                    }
-                  />
-                </HStack>
-              </Tag>
-            </LinkOverlay>
-          </LinkBox>
-        ))}
+      {variantsExcludingSelf.map(variant => (
+        <LinkBox key={`variant-${variant.name}`}>
+          <LinkOverlay as={NextLink} href={`/decks/${variant.id}`}>
+            <Tag>
+              <HStack spacing={1}>
+                <SpriteDisplay
+                  squishWidth
+                  pokemonNames={
+                    variant.defined_pokemon.length > 1 &&
+                    variant.defined_pokemon[1].length > 1
+                      ? [variant.defined_pokemon[1]]
+                      : [variant.defined_pokemon[0]]
+                  }
+                />
+              </HStack>
+            </Tag>
+          </LinkOverlay>
+        </LinkBox>
+      ))}
     </HStack>
   );
 });
