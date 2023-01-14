50a64398b6c482f0231e3a107a4ec480b4bb731e
Jared Grimes <jaredlgrimes@hotmail.com>
2023-01-13 20:29:33 -0500

    sorted copies where highest number is on top

--- src/components/Deck/ListViewer/CardViewer.tsx/CardViewerBody.tsx
+++ src/components/Deck/ListViewer/CardViewer.tsx/CardViewerBody.tsx
@@ -63,8 +63,8 @@ export const CardViewerBody = memo(
     );
 
     const cardCountsSorted = Object.entries(cardCounts).sort((a, b) => {
-      if (a[1] > b[1]) return -1;
-      if (b[1] > a[1]) return 1;
+      if (a[0] > b[0]) return -1;
+      if (b[0] < a[0]) return 1;
       return 0;
     });
 
