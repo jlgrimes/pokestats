22ff85bdd7487c79b450dd0cdbc546159bb99254
Jared Grimes <jaredlgrimes@hotmail.com>
2023-01-15 00:24:30 -0500

    fixed year showing up twice

--- src/lib/tournament.ts
+++ src/lib/tournament.ts
@@ -3,7 +3,13 @@ import { Tournament } from '../../types/tournament';
 
 export const shortenTournamentName = (tournament: Tournament) => {
   const tournamentStartYear = getYear(parseISO(tournament.date.start));
-  const shortName = tournament.name.replace('Pokémon TCG', '');
+  const shortName = tournament.name
+    .replace('Pokémon TCG', '')
+    .replace('TCG', '');
 
-  return `${shortName} ${tournamentStartYear}`;
+  return `${shortName} ${
+    shortName.includes(tournamentStartYear.toString())
+      ? ''
+      : tournamentStartYear
+  }`;
 };
