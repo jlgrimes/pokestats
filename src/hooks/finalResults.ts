5cde2caa36b92a62e62ab4f3ec60b325c345e4c5
Jared Grimes <jaredlgrimes@hotmail.com>
2023-01-16 11:16:54 -0500

    fixed card count sorting

--- src/hooks/finalResults.ts
+++ src/hooks/finalResults.ts
@@ -247,8 +247,8 @@ export const useCardCounts = (
   );
 
   const cardCountsSorted = Object.entries(cardCounts).sort((a, b) => {
-    if (a[0] > b[0]) return -1;
-    if (b[0] < a[0]) return 1;
+    if (a[1] > b[1]) return -1;
+    if (b[1] < a[1]) return 1;
     return 0;
   });
 
