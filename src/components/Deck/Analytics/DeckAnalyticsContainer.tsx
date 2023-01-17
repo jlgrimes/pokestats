5ad5c0705b1759d17ded6c9abac68e1fe07c544c
Jared Grimes <jaredlgrimes@hotmail.com>
2023-01-16 22:05:24 -0500

    fixed back to decks button padding

--- src/components/Deck/Analytics/DeckAnalyticsContainer.tsx
+++ src/components/Deck/Analytics/DeckAnalyticsContainer.tsx
@@ -1,4 +1,4 @@
-import { Stack } from '@chakra-ui/react';
+import { Stack, StackItem } from '@chakra-ui/react';
 import { Deck } from '../../../../types/tournament';
 import { BackToDecksButton } from './BackToDecksButton';
 import { DeckHeader } from './DeckHeader';
@@ -13,7 +13,9 @@ export const DeckAnalyticsContainer = ({
   compactTitle?: boolean;
 }) => (
   <Stack>
-    {!compactTitle && <BackToDecksButton />}
+    <StackItem padding={4} paddingBottom={0}>
+      {!compactTitle && <BackToDecksButton />}
+    </StackItem>
     <DeckHeader deck={deck} compact={compactTitle} />
     <Stack paddingX={4}>{children}</Stack>
   </Stack>
