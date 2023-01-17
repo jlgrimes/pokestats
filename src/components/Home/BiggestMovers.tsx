4b3bc35bf1f8cd96fac3ecefe3bffc08603e625b
Jared Grimes <jaredlgrimes@hotmail.com>
2023-01-16 22:36:45 -0500

    added back the title

--- src/components/Home/BiggestMovers.tsx
+++ src/components/Home/BiggestMovers.tsx
@@ -7,7 +7,7 @@ export const BiggestMovers = memo(
   ({ tournament }: { tournament: Tournament }) => {
     const tourneyId = parseInt(tournament.id);
     return (
-      <CommonCard slug={`/decks`} ghost>
+      <CommonCard header='Big moves 🏃' slug={`/decks`} ghost>
         <MetaGameShareList
           tournamentRange={[tourneyId, tourneyId]}
           sortByMoves
