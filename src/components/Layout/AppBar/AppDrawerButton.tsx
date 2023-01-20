9a1996e5d9fd16ad62f06c94f1728a53a8492e9c
Jared Grimes <jaredlgrimes@hotmail.com>
2023-01-19 23:49:53 -0500

    made app drawer better and profile link right

--- src/components/Layout/AppBar/AppDrawerButton.tsx
+++ src/components/Layout/AppBar/AppDrawerButton.tsx
@@ -1,23 +1,16 @@
 import {
   Drawer,
-  DrawerBody,
-  DrawerFooter,
-  DrawerHeader,
   DrawerOverlay,
   DrawerContent,
-  DrawerCloseButton,
-  Button,
   useDisclosure,
   IconButton,
   Stack,
-  Text,
   Link,
   Heading,
-  Divider,
   Grid,
   Icon,
+  HStack,
 } from '@chakra-ui/react';
-import { signOut } from 'next-auth/react';
 import NextLink from 'next/link';
 import { useRef } from 'react';
 import {
@@ -29,7 +22,6 @@ import {
 } from 'react-icons/fa';
 import { CombinedPlayerProfile } from '../../../../types/player';
 import { useUserIsAdmin } from '../../../hooks/administrators';
-import { parseUsername } from '../../../lib/strings';
 import { AccountRequestLink } from '../AccountRequestsLink';
 import { LogInOutButton } from './LogInOutButton';
 
@@ -62,37 +54,40 @@ export const AppDrawerButton = ({
         <DrawerContent>
           <Stack justifyContent='space-between' height='100%'>
             <Grid
-              templateColumns='1fr 1fr'
+              paddingTop={'4.5rem'}
+              templateColumns='0.6fr 1fr'
               rowGap={'1.5rem'}
-              padding='4.5rem 4.25rem 4.5rem 3.25rem'
               alignItems={'center'}
             >
-              <Icon as={FaRegCalendar} />
+              <HStack justifyContent={'end'} paddingRight={10}>
+                <Icon as={FaRegCalendar} />
+              </HStack>
               <Link as={NextLink} href='/tournaments' onClick={onClose}>
                 <Heading size='lg'>Tournaments</Heading>
               </Link>
 
-              <Icon as={FaRegListAlt} />
+              <HStack justifyContent={'end'} paddingRight={10}>
+                <Icon as={FaRegListAlt} />
+              </HStack>
+
               <Link as={NextLink} href='/decks' onClick={onClose}>
                 <Heading size='lg'>Decks</Heading>
               </Link>
 
-              <Icon as={FaRegUser} />
-              <Link
-                as={NextLink}
-                href={
-                  userProfile
-                    ? `/player/${parseUsername(userProfile.email)}`
-                    : `/setup-profile`
-                }
-                onClick={onClose}
-              >
+              <HStack justifyContent={'end'} paddingRight={10}>
+                <Icon as={FaRegUser} />
+              </HStack>
+
+              <Link as={NextLink} href={'/profile'} onClick={onClose}>
                 <Heading size='lg'>
                   {userProfile ? 'My profile' : 'Setup profile'}
                 </Heading>
               </Link>
 
-              <Icon as={FaRegQuestionCircle} />
+              <HStack justifyContent={'end'} paddingRight={10}>
+                <Icon as={FaRegQuestionCircle} />
+              </HStack>
+
               <Link as={NextLink} href={'/about'} onClick={onClose}>
                 <Heading size='lg'>About</Heading>
               </Link>
