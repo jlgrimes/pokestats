7718db3194a20ab65d975098604361ab8bec4549
Jared Grimes <jaredlgrimes@hotmail.com>
2023-01-30 23:00:49 -0500

    made load results api update, broken but updated liverpool i think?

--- pages/api/load-all-final-results.ts
+++ pages/api/load-all-final-results.ts
@@ -14,12 +14,15 @@ export default async function handler(
     const tournaments = await fetchTournaments({ prefetch: true });
     for await (const tournament of tournaments) {
       console.log('Loading results for', tournament.name, '...');
-      const { error } = await loadFinalResults(tournament.id);
-      if (error) return res.status(500);
+      const { error } = await loadFinalResults(tournament.id, true);
+      if (error) {
+        console.log(error);
+        return res.status(500);
+      }
     }
     return res.status(200);
   } catch (err) {
-    console.log(err)
+    console.log(err);
     return res.status(500);
   } finally {
     res.end();
--- src/lib/supabase/finalResults.ts
+++ src/lib/supabase/finalResults.ts
@@ -1,14 +1,18 @@
 import { fetchLiveResults } from '../fetch/fetchLiveResults';
 import supabase from './client';
 
-export const loadFinalResults = async (tournamentId: string) => {
+export const loadFinalResults = async (
+  tournamentId: string,
+  shouldUpdate?: boolean
+) => {
   const { data: finalResultsData } = await supabase
     .from('Final Results')
-    .select('id')
+    .select('id,created_at,name')
     .eq('tournament_id', tournamentId);
 
   // We've already inserted final results for this tournament.
-  if (finalResultsData && finalResultsData.length > 0) {
+  const dataExists = finalResultsData && finalResultsData.length > 0;
+  if (dataExists && !shouldUpdate) {
     console.log('We already inserted the results. Cancelling...');
     return { error: null };
   }
@@ -27,18 +31,45 @@ export const loadFinalResults = async (tournamentId: string) => {
     return { error: null };
   }
 
-  // Fire away!
-  const rowsToBeInserted = playerData.map(player => ({
-    tournament_id: tournamentId,
-    name: player.name,
-    placing: player.placing,
-    record: player.record,
-    resistances: player.resistances,
-    deck_list: player.deck?.list,
-    deck_archetype: player.deck?.list ?  player.deck.id : null,
-    rounds: player.rounds,
-  }));
+  if (dataExists) {
+    const rowsToBeUpserted = finalResultsData.map(finalResult => {
+      const player = playerData.find(({ name }) => name === finalResult.name);
 
-  const result = await supabase.from('Final Results').insert(rowsToBeInserted);
-  return result;
+      if (!player) return {};
+
+      return {
+        ...finalResult,
+        name: player.name,
+        placing: player.placing,
+        record: player.record,
+        resistances: player.resistances,
+        deck_list: player.deck?.list ?? null,
+        deck_archetype: player.deck?.list ? player.deck.id : null,
+        rounds: player.rounds,
+        tournament_id: tournamentId
+      };
+    });
+
+    const result = await supabase
+      .from('Final Results')
+      .upsert(rowsToBeUpserted);
+    return result;
+  } else {
+    // Fire away!
+    const rowsToBeInserted = playerData.map(player => ({
+      tournament_id: tournamentId,
+      name: player.name,
+      placing: player.placing,
+      record: player.record,
+      resistances: player.resistances,
+      deck_list: player.deck?.list ?? null,
+      deck_archetype: player.deck?.list ? player.deck.id : null,
+      rounds: player.rounds,
+    }));
+
+    const result = await supabase
+      .from('Final Results')
+      .insert(rowsToBeInserted);
+    return result;
+  }
 };
