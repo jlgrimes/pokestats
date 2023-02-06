fb05a4963d9423e2a62d643da11caaa3d6967189
Jared Grimes <jaredlgrimes@hotmail.com>
2023-02-06 14:34:08 -0500

    prevents static loading of standings

--- pages/tournaments/[id]/standings.tsx
+++ pages/tournaments/[id]/standings.tsx
@@ -22,42 +22,14 @@ export default function TournamentPage({
 }
 
 export async function getStaticProps({ params }: { params: { id: string } }) {
-  const queryClient = new QueryClient();
-  const currentLiveResults = await fetchLiveResults(params.id, {
-    prefetch: true,
-    load: { allRoundData: true },
-  });
-
-  queryClient.setQueryData(
-    [`live-results`, params.id, 'allRoundData', true],
-    () => currentLiveResults
-  );
-
-  // TODO: take out, might not need
-  await queryClient.prefetchQuery([`pokedex`], fetchPokedex);
-  await queryClient.prefetchQuery(['deck-archetypes'], fetchArchetypes);
-  // TODO: update with other tournament metadata if needed
-  await queryClient.prefetchQuery(
-    ['tournament-metadata', params.id, 'stream'],
-    () => fetchTournamentMetadata(params.id, 'stream')
-  );
-
-  let [tournament] = await fetchTournaments({
+  const [tournament] = await fetchTournaments({
     tournamentId: params.id,
     prefetch: true,
   });
-  queryClient.setQueryData(['tournaments', params.id], () => tournament);
-
-  tournament = (await getPatchedTournament(
-    tournament,
-    currentLiveResults,
-    true
-  )) as Tournament;
 
   return {
     props: {
       tournament,
-      dehydratedState: dehydrate(queryClient),
     },
     revalidate: 10,
   };
