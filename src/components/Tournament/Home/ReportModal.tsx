6bb16588f26fb4a81d8baa93652792f6028ffc63
Jared Grimes <jaredlgrimes@hotmail.com>
2023-02-16 18:17:24 -0500

    made report reset every time

--- src/components/Tournament/Home/ReportModal.tsx
+++ src/components/Tournament/Home/ReportModal.tsx
@@ -1,7 +1,9 @@
 import { useDisclosure, UseDisclosureProps } from '@chakra-ui/react';
-import { Fragment } from 'react';
-import { Tournament } from '../../../../types/tournament';
+import { Fragment, useState } from 'react';
+import { Deck, Tournament } from '../../../../types/tournament';
+import { useUserIsAdmin } from '../../../hooks/administrators';
 import { useLiveTournamentPlayers } from '../../../hooks/tournamentResults';
+import { ArchetypeSelectorModal } from '../../Deck/DeckInput/ArchetypeSelector/ArchetypeSelectorModal';
 import { PlayerSelectModal } from './PinnedPlayers/PlayerSelectModal';
 
 interface ReportModalProps {
@@ -11,20 +13,43 @@ interface ReportModalProps {
 
 export const ReportModal = (props: ReportModalProps) => {
   const { data: playerNames } = useLiveTournamentPlayers(props.tournament.id);
+  const { data: userIsAdmin } = useUserIsAdmin();
 
-  const handlePlayerSelect = () => {
+  const [selectedPlayer, setSelectedPlayer] = useState<string | undefined>();
+  const archetypeModalControls = useDisclosure();
+
+  const handlePlayerSelect = (name: string) => {
+    setSelectedPlayer(name);
     props.playerSelectModalControls.onClose &&
       props.playerSelectModalControls.onClose();
+
+    archetypeModalControls.onOpen();
+  };
+
+  const handleDeckSelect = (deck: Deck) => {
+    console.log(selectedPlayer, deck);
   };
 
   return (
     <Fragment>
-      <PlayerSelectModal
-        tournament={props.tournament}
-        modalControls={props.playerSelectModalControls}
-        playerNames={playerNames ?? []}
-        handleSubmit={handlePlayerSelect}
-      />
+      {(props.playerSelectModalControls.isOpen ||
+        archetypeModalControls.isOpen) && (
+        <Fragment>
+          <PlayerSelectModal
+            tournament={props.tournament}
+            modalControls={props.playerSelectModalControls}
+            playerNames={playerNames ?? []}
+            handleSubmit={handlePlayerSelect}
+          />
+          <ArchetypeSelectorModal
+            onChange={handleDeckSelect}
+            tournamentId={props.tournament.id}
+            modalControls={archetypeModalControls}
+            userIsAdmin={userIsAdmin}
+            isListUp={false}
+          />
+        </Fragment>
+      )}
     </Fragment>
   );
 };
