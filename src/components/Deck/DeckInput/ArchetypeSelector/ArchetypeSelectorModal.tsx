89704b82dc122954a650fcc3246c3615f3c8db3d
Jared Grimes <jaredlgrimes@hotmail.com>
2023-02-16 18:43:15 -0500

    added stream check

--- src/components/Deck/DeckInput/ArchetypeSelector/ArchetypeSelectorModal.tsx
+++ src/components/Deck/DeckInput/ArchetypeSelector/ArchetypeSelectorModal.tsx
@@ -68,7 +68,7 @@ export const ArchetypeSelectorModal = memo((props: ArchetypeSelectorProps) => {
         <ModalCloseButton />
         <ModalBody>
           <Stack spacing={4}>
-            {props.userIsAdmin && props.isStreamDeck && (
+            {props.userIsAdmin && props.toggleIsStreamDeck && (
               <Checkbox
                 isChecked={props.isStreamDeck}
                 onChange={props.toggleIsStreamDeck}
