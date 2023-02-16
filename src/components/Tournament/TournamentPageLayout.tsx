46fd99cfb2fd9f3d989bf76c7e3ee6b56d06f309
Jared Grimes <jaredlgrimes@hotmail.com>
2023-02-16 10:56:44 -0500

    shortened layout for tournament page

--- src/components/Tournament/TournamentPageLayout.tsx
+++ src/components/Tournament/TournamentPageLayout.tsx
@@ -1,6 +1,8 @@
 import { Badge, Box, Heading, HStack, Stack } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
 import { ReactNode } from 'react';
 import { Tournament } from '../../../types/tournament';
+import { capitalize } from '../../lib/strings';
 import {
   getTournamentStatusBadgeProps,
   formatTournamentStatus,
@@ -15,18 +17,21 @@ export const TournamentPageLayout = ({
   children: ReactNode;
   tournament: Tournament | null;
 }) => {
+  const router = useRouter();
+  const slug = router.asPath.split('/').at(router.asPath.split('/').length - 1);
+
   if (!tournament) return null;
 
   return (
     <Stack spacing={0} flexGrow={1} overflow='hidden'>
-      <Stack paddingX={4} paddingTop={2}>
+      <Stack paddingX={4} paddingTop={4}>
         <Heading size='lg' color='gray.700'>
-          {tournament.name}
+          {`${tournament.name} ${capitalize(slug as string)}`}
         </Heading>
-        <Stack>
+        {/* <Stack>
           <TournamentLinks tournament={tournament} />
           <TournamentTabs tournament={tournament} />
-        </Stack>
+        </Stack> */}
       </Stack>
       {children}
     </Stack>
