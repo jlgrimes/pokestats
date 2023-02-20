af02db43f7b6facc7c7cddbf9d461167d892fb54
Jared Grimes <jaredlgrimes@hotmail.com>
2023-02-20 13:05:13 -0500

    replace country flag with next image

--- src/components/Tournament/Home/CountryFlag.tsx
+++ src/components/Tournament/Home/CountryFlag.tsx
@@ -1,4 +1,5 @@
 import { Box, Image } from '@chakra-ui/react';
+import NextImage from 'next/image';
 
 interface CountryFlagProps {
   countryCode: string;
@@ -9,13 +10,13 @@ export const CountryFlag = (props: CountryFlagProps) => {
   const url = `https://countryflagsapi.com/png/${props.countryCode}`;
 
   return (
-    <Box>
-      <Image
-        opacity='0.85'
+    <Box opacity='0.85'>
+      <NextImage
         crossOrigin='anonymous'
         alt={`${props.countryCode} flag`}
         src={url}
-        height={`${props.smol ? 20 : 25}px`}
+        height={props.smol ? 20 : 25}
+        width={props.smol ? 40 : 50}
       />
     </Box>
   );
