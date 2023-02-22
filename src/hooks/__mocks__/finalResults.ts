0aaf9db10048d2b65039695429d81cc1b7f3f308
Jared Grimes <jaredlgrimes@hotmail.com>
2023-02-22 15:59:22 -0500

    added mock champion

---
+++ src/hooks/__mocks__/finalResults.ts
@@ -0,0 +1,22 @@
+export const useChampions = () => ({
+  data: {
+    '2': {
+      name: 'Noah Conway',
+      placing: 1,
+      record: {
+        wins: 15,
+        ties: 3,
+        losses: 0,
+      },
+      resistances: {},
+      rounds: [],
+      tournament_id: '2',
+      deck_list: {},
+      deck_archetype: {
+        id: 1,
+        name: 'Torterra',
+        defined_pokemon: ['Torterra'],
+      },
+    },
+  },
+});
