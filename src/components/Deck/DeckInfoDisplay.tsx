6bd1cec24761d087422f0bd0c7903304617eff40
Jared Grimes <jaredlgrimes@hotmail.com>
2023-03-09 11:59:26 -0500

    UI for report inline

--- src/components/Deck/DeckInfoDisplay.tsx
+++ src/components/Deck/DeckInfoDisplay.tsx
@@ -1,8 +1,10 @@
 import {
+  Box,
   Grid,
   IconButton,
   Stack,
   StackItem,
+  Text,
   useDisclosure,
 } from '@chakra-ui/react';
 import { useSession } from 'next-auth/react';
@@ -45,20 +47,77 @@ export const DeckInfoDisplay = memo(
         gridTemplateColumns={shouldShowAsText ? 'auto 1fr 1fr' : '85px 1fr 1fr'}
         alignItems='center'
       >
-        <StackItem>
-          <DeckInput
-            tournamentId={tournament.id}
-            playerName={player.name}
-            deck={player.deck ?? undefined}
-            archetypeModal={archetypeModal}
-            shouldShowAsText={shouldShowAsText}
-            shouldHideDeck={
-              shouldHideDeck &&
-              session.data?.user?.email !== player.deck?.user_who_submitted
-            }
-            shouldHideVerifiedIcon={shouldHideVerifiedIcon}
-          />
-        </StackItem>
+        <Box
+          position={'relative'}
+          {...(enableEdits
+            ? {
+                outline: '2px solid',
+                outlineColor: player.deck?.id ? 'pink.100' : 'pink.500',
+                rounded: 'md',
+              }
+            : {})}
+        >
+          {!(enableEdits && !player.deck?.id) ? (
+            <DeckInput
+              tournamentId={tournament.id}
+              playerName={player.name}
+              deck={player.deck ?? undefined}
+              archetypeModal={archetypeModal}
+              shouldShowAsText={shouldShowAsText}
+              shouldHideDeck={
+                shouldHideDeck &&
+                session.data?.user?.email !== player.deck?.user_who_submitted
+              }
+              shouldHideVerifiedIcon={shouldHideVerifiedIcon}
+            />
+          ) : (
+            <Box
+              height='30px'
+              paddingY={0.5}
+              display='flex'
+              alignItems='center'
+              justifyContent={'center'}
+              gap={1}
+            >
+              <Text
+                fontSize={12}
+                textTransform={'uppercase'}
+                fontWeight='bold'
+                color='pink.500'
+              >
+                Report
+              </Text>
+              <IconButton
+                icon={<FaRegEdit />}
+                aria-label='edit'
+                variant={'ghost'}
+                width={4}
+                minWidth={0}
+                size='sm'
+                color='pink.500'
+                paddingX={0}
+                onClick={archetypeModal.onOpen}
+              />
+            </Box>
+          )}
+          {enableEdits && player.deck?.id && (
+            <IconButton
+              zIndex={5}
+              position={'absolute'}
+              top={-3}
+              right={-2}
+              maxWidth={'2'}
+              icon={<FaRegEdit />}
+              aria-label='edit'
+              variant={'ghost'}
+              width={'100%'}
+              size='sm'
+              color='pink.300'
+              paddingRight={0}
+              onClick={archetypeModal.onOpen}
+            />
+          )}
+        </Box>
 
         {player?.deck?.list && !disableList && (
           <ListViewerOpenButton result={player} tournament={tournament} />
@@ -78,19 +137,7 @@ export const DeckInfoDisplay = memo(
         {/* {player?.deck?.list && !disableList && (
           <ListViewerOpenButton result={player} tournament={tournament} />
         )}
-        {enableEdits && (
-          <IconButton
-            maxWidth={'2'}
-            icon={<FaRegEdit />}
-            aria-label='edit'
-            variant={'ghost'}
-            width={'100%'}
-            size='md'
-            color='gray.400'
-            paddingRight={0}
-            onClick={archetypeModal.onOpen}
-          />
-        )} */}
+         */}
       </Grid>
     );
   }
