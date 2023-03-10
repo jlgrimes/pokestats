9974e9bd078ee25f7d06c99959ff16a958da7c5a
Jared Grimes <jaredlgrimes@hotmail.com>
2023-03-10 14:01:21 -0500

    APP BAR

--- src/components/Layout/AppBar/AppBar.tsx
+++ src/components/Layout/AppBar/AppBar.tsx
@@ -13,7 +13,6 @@ import { useSessionUserProfile } from '../../../hooks/user';
 import { NotVerifiedIcon, VerifiedIcon } from '../../Player/Icons';
 import { AppDrawerButton, UserStatus } from './AppDrawerButton';
 import { AppLogo } from './AppLogo';
-import { SHOULD_SHOW_COMING_SOON } from '../../../lib/coming-soon';
 import React, { useCallback } from 'react';
 import { useRouter } from 'next/router';
 import { StickyHeader } from '../../common/Layout/StickyHeader';
@@ -41,50 +40,47 @@ export const AppBar = () => {
         direction={'row'}
         alignItems={'center'}
         padding={'0.25rem 1.5rem'}
-        justifyContent={SHOULD_SHOW_COMING_SOON ? 'center' : 'space-between'}
+        justifyContent={'space-between'}
       >
-        {!SHOULD_SHOW_COMING_SOON && (
-          <AppDrawerButton userStatus={getUserStatus()} />
-        )}
+        {<AppDrawerButton userStatus={getUserStatus()} />}
         {router.asPath !== '/' && router.asPath !== '/help' && <AppLogo />}
-        {!SHOULD_SHOW_COMING_SOON &&
-          (session.status !== 'unauthenticated' ? (
-            <>
-              <LinkBox>
-                <LinkOverlay
-                  as={NextLink}
-                  href={userProfile ? `/profile` : `/setup-profile`}
-                >
-                  <Stack direction={'row'} alignItems='end' spacing={-1.5}>
-                    {!session.data?.user?.image ? (
-                      <SkeletonCircle size='8' />
-                    ) : (
-                      <Avatar
-                        size='sm'
-                        name={session.data?.user?.name ?? undefined}
-                        src={session.data?.user?.image}
-                      />
-                    )}
-                    {userProfile ? (
-                      <VerifiedIcon />
-                    ) : profileIsLoading ? (
-                      <SkeletonCircle size='4' />
-                    ) : (
-                      <NotVerifiedIcon />
-                    )}
-                  </Stack>
-                </LinkOverlay>
-              </LinkBox>
-            </>
-          ) : (
-            <IconButton
-              size={'sm'}
-              variant='outline'
-              onClick={() => signIn('google')}
-              aria-label='log in'
-              icon={<FaUser />}
-            />
-          ))}
+        {session.status !== 'unauthenticated' ? (
+          <>
+            <LinkBox>
+              <LinkOverlay
+                as={NextLink}
+                href={userProfile ? `/profile` : `/setup-profile`}
+              >
+                <Stack direction={'row'} alignItems='end' spacing={-1.5}>
+                  {!session.data?.user?.image ? (
+                    <SkeletonCircle size='8' />
+                  ) : (
+                    <Avatar
+                      size='sm'
+                      name={session.data?.user?.name ?? undefined}
+                      src={session.data?.user?.image}
+                    />
+                  )}
+                  {userProfile ? (
+                    <VerifiedIcon />
+                  ) : profileIsLoading ? (
+                    <SkeletonCircle size='4' />
+                  ) : (
+                    <NotVerifiedIcon />
+                  )}
+                </Stack>
+              </LinkOverlay>
+            </LinkBox>
+          </>
+        ) : (
+          <IconButton
+            size={'sm'}
+            variant='outline'
+            onClick={() => signIn('google')}
+            aria-label='log in'
+            icon={<FaUser />}
+          />
+        )}
       </Stack>
     </StickyHeader>
   );
