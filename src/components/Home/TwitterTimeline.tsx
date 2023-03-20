9e0a6296c93dcfcd2c7e7022c6492bf1b8122e05
Jared Grimes <jaredlgrimes@hotmail.com>
2023-03-19 21:59:42 -0400

    twitter dark mode on load

--- src/components/Home/TwitterTimeline.tsx
+++ src/components/Home/TwitterTimeline.tsx
@@ -1,9 +1,14 @@
-import { Box, Link } from '@chakra-ui/react';
+import { Box, Link, useColorMode } from '@chakra-ui/react';
 
-export const TwitterTimeline = () => (
-  <Box
-    as={Link}
-    className='twitter-timeline'
-    href='https://twitter.com/pokestatstcg?ref_src=twsrc%5Etfw'
-  ></Box>
-);
+export const TwitterTimeline = () => {
+  const { colorMode } = useColorMode();
+
+  return (
+    <Box
+      as={Link}
+      className='twitter-timeline'
+      href='https://twitter.com/pokestatstcg?ref_src=twsrc%5Etfw'
+      data-theme={colorMode}
+    ></Box>
+  );
+};
