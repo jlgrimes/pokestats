0b8d76f3e89f5293df3be3a853ec115557216658
Jared Grimes <jaredlgrimes@hotmail.com>
2023-03-20 19:03:47 -0400

    added live logo

--- pages/player/[id].tsx
+++ pages/player/[id].tsx
@@ -37,14 +37,12 @@ export default function Page({ username }: { username: string }) {
           name='twitter:title'
           content={`Follow ${username} on PokéStats Live`}
         />
+        <meta name='twitter:image' content='/live.png' />
         {/* <meta
           name='twitter:description'
           content={`View ${username}'s player profile on PokéStats Live.`}
         />
-        <meta
-          name='twitter:image'
-          content='http://1.bp.blogspot.com/-RqEPz32fWg0/XYveSw2AQbI/AAAAAAAAPtY/HQ9ivK9BRSQTT433qkQ9Pq4RypyIj2UUgCK4BGAYYCw/s1218/4.jpg'
-        /> */}
+         */}
       </Head>
       <PlayerProfilePage profile={data} />
     </div>
