33c247cbd26414231af67aee2efe7ca3113688c3
Jared Grimes <jaredlgrimes@hotmail.com>
2023-03-21 10:10:03 -0400

    prevent anyone from editing anyone else's decks

--- src/components/DataDisplay/PlayerPerformanceList.tsx
+++ src/components/DataDisplay/PlayerPerformanceList.tsx
@@ -77,7 +77,7 @@ export const PlayerPerformanceList = ({
                   player={performance}
                   tournament={tournament}
                   shouldHideDecks={false}
-                  canEditDecks
+                  canEditDecks={userMatchesLoggedInUser || userIsAdmin}
                 />
               </Box>
             </Stack>
