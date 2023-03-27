b22e2c131139de176070f2fb3d6a25830cf84107
Jared Grimes <jaredlgrimes@hotmail.com>
2023-03-27 17:41:44 -0400

    donate!

--- src/components/Home/HomeIcons.tsx
+++ src/components/Home/HomeIcons.tsx
@@ -1,7 +1,8 @@
 import NextLink from 'next/link';
 import { Button, ButtonGroup, HStack } from '@chakra-ui/react';
 import { IconCards } from '@tabler/icons-react';
-import { FaRegQuestionCircle } from 'react-icons/fa';
+import { IconGift } from '@tabler/icons-react';
+import { FaGift, FaHeart, FaRegQuestionCircle } from 'react-icons/fa';
 
 export const HomeIcons = () => (
   <HStack justifyContent={'center'}>
@@ -11,6 +12,15 @@ export const HomeIcons = () => (
       </Button>
       <Button as={NextLink} href='/help' leftIcon={<FaRegQuestionCircle />}>
         Help
+      </Button>
+      <Button
+        colorScheme='pink'
+        as={NextLink}
+        href='https://t.co/ruYHo54RjN'
+        target='_blank'
+        leftIcon={<FaHeart />}
+      >
+        Donate
       </Button>
     </ButtonGroup>
   </HStack>
