a7f90c5f6e6cd8f84567b59ba96c1843da9619d9
Jared Grimes <jaredlgrimes@hotmail.com>
2023-03-27 19:42:13 -0400

    ads i guess

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import { ColorModeScript } from '@chakra-ui/react';
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 import theme from '../styles/theme';
 
 export default class Document extends NextDocument {
@@ -10,6 +11,11 @@ export default class Document extends NextDocument {
         <body>
           {/* 👇 Here's the script */}
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <Script
+            async
+            src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3066736963130742'
+            crossOrigin='anonymous'
+          />
           <Main />
           <NextScript />
         </body>
