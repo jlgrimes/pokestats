350e833c45002c1345417b54de07df699d16d0f0
Jared Grimes <jaredlgrimes@hotmail.com>
2023-04-05 08:40:12 -0400

    unique file upload and adds ext

--- src/components/Deck/ImageListViewer/UploadListButton.tsx
+++ src/components/Deck/ImageListViewer/UploadListButton.tsx
@@ -7,9 +7,25 @@ import {
 } from '@chakra-ui/react';
 import { IconFileUpload } from '@tabler/icons-react';
 import { useRef } from 'react';
+import supabase from '../../../lib/supabase/client';
+import { v4 as uuidv4 } from 'uuid';
 
 export const UploadListButton = () => {
   const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleFileUpload = async (e: Event) => {
+    const file = (e.target as HTMLInputElement).files?.[0];
+    if (!file) return console.error('No file');
+
+    const ext = file.name.split('.')[1];
+    const fileName = uuidv4()
+    const { data, error } = await supabase.storage
+      .from('uploaded-deck-lists')
+      .upload(`${fileName}.${ext}`, file, {
+        cacheControl: '3600',
+        upsert: false,
+      });
+  };
 
   return (
     <FormControl isRequired>
@@ -29,6 +45,7 @@ export const UploadListButton = () => {
           onClick={e => {
             e.stopPropagation();
           }}
+          onChange={handleFileUpload}
           // name={name}
           // ref={inputRef}
           // {...inputProps}
