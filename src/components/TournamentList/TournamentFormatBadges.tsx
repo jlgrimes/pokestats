8c67276a793fb36477fe07e7695dc33d26e6a9f5
Jared Grimes <jaredlgrimes@hotmail.com>
2023-04-21 17:34:44 -0400

    fine no outline

--- src/components/TournamentList/TournamentFormatBadges.tsx
+++ src/components/TournamentList/TournamentFormatBadges.tsx
@@ -3,10 +3,10 @@ import { TournamentStatusBadgeProps } from './TournamentStatusBadge';
 
 export const TournamentFormatBadges = (props: TournamentStatusBadgeProps) => (
   <HStack paddingY={1}>
-    <Badge variant='outline' fontSize={props.size}>
+    <Badge fontSize={props.size}>
       {props.tournament.format.rotation} Block-on
     </Badge>
-    <Badge variant='outline' fontSize={props.size}>
+    <Badge fontSize={props.size}>
       {props.tournament.format.format}
     </Badge>
   </HStack>
