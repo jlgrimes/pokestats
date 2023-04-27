cd38556e28a95bbae99bfb559d435e380a63490d
Jared Grimes <jaredlgrimes@hotmail.com>
2023-04-27 15:57:53 -0400

    added ad code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,11 @@ export default class Document extends NextDocument {
     return (
       <Html lang='en'>
         <Head />
+        <script
+          async
+          src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3066736963130742'
+          crossOrigin='anonymous'
+        ></script>
         <body>
           {/* 👇 Here's the script */}
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
