d594f5295c2b1f095962de17c91a0a603d42543f
Jared Grimes <jaredlgrimes@hotmail.com>
2023-05-01 10:33:03 -0400

    removed back red

--- src/components/Home/HomeAd.tsx
+++ src/components/Home/HomeAd.tsx
@@ -2,7 +2,7 @@ import { Box } from '@chakra-ui/react';
 import { Adsense } from '@ctrl/react-adsense';
 
 export const HomeAd = ({ slot }: { slot?: string }) => (
-  <Box backgroundColor={'red'}>
-  <Adsense client='ca-pub-3066736963130742' slot={slot ?? '5287940290'} />
+  <Box>
+    <Adsense client='ca-pub-3066736963130742' slot={slot ?? '5287940290'} />
   </Box>
 );
