9742874de60fbe0ab9f4169737d3f3b0d7d2c596
Jared Grimes <jaredlgrimes@hotmail.com>
2023-05-06 14:35:20 +0200

    hotfix for bad tournaments

--- pages/api/load-tournaments.ts
+++ pages/api/load-tournaments.ts
@@ -12,6 +12,9 @@ type Data = {
   message: any;
 };
 
+// Outdated/incorrect tournaments that shouldn't exist
+const TOURNAMENTS_DISABLED_LIST = ['0000051', '0000052'];
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
@@ -22,20 +25,24 @@ export default async function handler(
 
     if (!tournaments || tournaments.length === 0) throw 'No tournaments.';
     const result = await supabase.from('Tournaments').upsert(
-      tournaments.map(tournament => ({
-        id: tournament.id,
-        name: tournament.name,
-        date: tournament.date,
-        tournamentStatus: tournament.tournamentStatus,
-        players: tournament.players,
-        winners: tournament.winners,
-        roundNumbers: tournament.roundNumbers,
-        rk9link: tournament.rk9link,
-        subStatus: tournament.subStatus,
-        format: formats
-          ? getTournamentFormat(formats, tournament)?.id ?? null
-          : null,
-      }))
+      tournaments
+        .filter(
+          tournament => !TOURNAMENTS_DISABLED_LIST.includes(tournament.id)
+        )
+        .map(tournament => ({
+          id: tournament.id,
+          name: tournament.name,
+          date: tournament.date,
+          tournamentStatus: tournament.tournamentStatus,
+          players: tournament.players,
+          winners: tournament.winners,
+          roundNumbers: tournament.roundNumbers,
+          rk9link: tournament.rk9link,
+          subStatus: tournament.subStatus,
+          format: formats
+            ? getTournamentFormat(formats, tournament)?.id ?? null
+            : null,
+        }))
     );
     if (result.error) throw result.error;
 
