d239db2709401bf89d11c4f53099ff30c6e852bc
Jared Grimes <jaredlgrimes@hotmail.com>
2023-07-06 11:00:43 -0500

    hopefully ad size is fixed

--- src/components/Ad.tsx
+++ src/components/Ad.tsx
@@ -2,5 +2,5 @@ import { Box } from '@chakra-ui/react';
 import { Adsense } from '@ctrl/react-adsense';
 
 export const Ad = ({ slot, height }: { slot?: string, height?: string }) => (
-  <Adsense client='ca-pub-3066736963130742' slot={slot ?? '5287940290'} {...(height ? { style: { display: 'block', height: '40px', width: '100%' } } : {})} />
+  <Adsense client='ca-pub-3066736963130742' slot={slot ?? '5287940290'} {...(height ? { style: { display: 'block', height: '40px', width: '100%' }, format: ''} : {})} />
 );
