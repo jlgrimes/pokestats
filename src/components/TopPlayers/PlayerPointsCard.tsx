9e97267541c06cef412da66eb6e0ace4115101b4
Jared Grimes <jaredlgrimes@hotmail.com>
2023-07-25 20:20:30 -0500

    fixed hyperlink

--- src/components/TopPlayers/PlayerPointsCard.tsx
+++ src/components/TopPlayers/PlayerPointsCard.tsx
@@ -17,9 +17,9 @@ export const PlayerPointsCard = (props: PlayerPointsCardProps) => {
     <Box>
       {props.player.profile && <PlayerJourneyModal  player={props.player} isOpen={isOpen} onClose={onClose} />}
       <Grid gridTemplateColumns={`1.5rem 2.6rem 1fr auto auto`} gap={4} alignItems='center'>
-        <Heading size='md' color='gray.800' textAlign='right' onClick={() => router.push(`/player/${props.player.profile?.username}`)}>{props.player.placing}</Heading>
+        <Heading size='md' color='gray.800' textAlign='right'>{props.player.placing}</Heading>
         <CountryFlag countryCode={props.player.country_code} size={'sm'} />
-        <Stack spacing={0}>
+        <Stack spacing={0} onClick={() => props.player.profile && router.push(`/player/${props.player.profile?.username}`)} cursor={props.player.profile ? 'pointer' : 'auto'}>
           <Text fontSize='lg' fontWeight='semibold'>{props.player.profile?.name ?? props.player.name}</Text>
           <Text fontWeight='semibold' color='gray.500'>{props.player.points} CP</Text>
         </Stack>
