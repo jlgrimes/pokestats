35aaa83b6e1b5e041866f343703cdee8fac0c7e3
Jared Grimes <jaredlgrimes@hotmail.com>
2023-07-25 19:33:34 -0500

    compacted points card

--- src/components/TopPlayers/PlayerPointsCard.tsx
+++ src/components/TopPlayers/PlayerPointsCard.tsx
@@ -17,21 +17,16 @@ export const PlayerPointsCard = (props: PlayerPointsCardProps) => {
     <Box>
       {props.player.profile && <PlayerJourneyModal  player={props.player} isOpen={isOpen} onClose={onClose} />}
       <Grid gridTemplateColumns={`1.5rem 2.6rem 1fr auto auto`} gap={4} alignItems='center'>
-        <Heading size='md' color='gray.800' textAlign='right'>{props.player.placing}</Heading>
+        <Heading size='md' color='gray.800' textAlign='right' onClick={() => router.push(`/player/${props.player.profile?.username}`)}>{props.player.placing}</Heading>
         <CountryFlag countryCode={props.player.country_code} size={'sm'} />
         <Stack spacing={0}>
-          <Text fontSize='lg' fontWeight='semibold'>{props.player.name}</Text>
-          <Text>{props.player.points} CP</Text>
+          <Text fontSize='lg' fontWeight='semibold'>{props.player.profile?.name ?? props.player.name}</Text>
+          <Text fontWeight='semibold' color='gray.500'>{props.player.points} CP</Text>
         </Stack>
         <Tooltip label={props.player.profile ? null : `${props.player.name} is not on pokestats :(`}>
-          <Stack direction={{ base: 'column', sm: 'row' }}>
             <Button leftIcon={<FaRoute />} size={{ base: 'sm', sm: 'md' }} isDisabled={!props.player.profile} onClick={onOpen}>
               Season
             </Button>
-            <Button leftIcon={<FaRegUser />} size={{ base: 'sm', sm: 'md' }} isDisabled={!props.player.profile} onClick={() => router.push(`/player/${props.player.profile?.username}`)}>
-              Profile
-            </Button>
-          </Stack>
         </Tooltip>
       </Grid>
     </Box>
