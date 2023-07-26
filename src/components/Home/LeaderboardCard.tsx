8c7c009eed83fb9c6125519ed2ff00fa9c1beee1
Jared Grimes <jaredlgrimes@hotmail.com>
2023-07-25 20:23:20 -0500

    changed copy

--- src/components/Home/LeaderboardCard.tsx
+++ src/components/Home/LeaderboardCard.tsx
@@ -3,7 +3,7 @@ import { TopPlayersList } from "../TopPlayers/TopPlayersList"
 
 export const LeaderboardCard = () => {
   return (
-    <CommonCard header='2023 Leaderboard' ghost>
+    <CommonCard header='Masters Leaderboard' subheader="2023, Global" ghost>
       <TopPlayersList isCompact season={46} />
     </CommonCard>
   )
