6d6128f088ff185de73807972aedf20dfac5cedf
BuildTools <unconfigured@null.spigotmc.org>
2023-10-01 23:36:20 -0500

    added flags!

--- src/hooks/newStandings.ts
+++ src/hooks/newStandings.ts
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query"
 import supabase from "../lib/supabase/client"
 import { capitalize } from "../lib/strings";
 import { Tournament } from "../../types/tournament";
+import { cropPlayerName, getPlayerRegion } from "../lib/fetch/fetchLiveResults";
 
 export const fixStoredDeck = (decklist: any) => {
   // if (decklist === '""') return {};
@@ -12,7 +13,11 @@ export const fixStoredDeck = (decklist: any) => {
 
 const fetchStandings = async (params: UseStandingsParams) => {
   const standings = await supabase.from(params.tournament.tournamentStatus === 'running' ? 'live_standings' : 'standings_new').select('*,deck_archetype(id,defined_pokemon)').eq('tournament_id', params.tournament.id).eq('age_division', capitalize(params.ageDivision)).order('placing', { ascending: true });
-  return standings.data;
+  return standings.data?.map((standing) => ({
+    ...standing,
+    name: cropPlayerName(standing.name),
+    region: getPlayerRegion(standing.name)?.at(1)
+  }));
 }
 
 interface UseStandingsParams {
