4f2718909b46a331dfba61f0d9ce8a42110c43fe
BuildTools <unconfigured@null.spigotmc.org>
2023-10-06 15:52:59 -0500

    changed live in banner

--- src/components/TournamentList/TournamentStatusBadges.tsx
+++ src/components/TournamentList/TournamentStatusBadges.tsx
@@ -4,16 +4,20 @@ import {
   MoonIcon,
   StatusOnlineIcon
 } from "@heroicons/react/outline";
+import { formatTournamentStatus } from "./helpers";
+import { useUtcOffset } from "../../hooks/tournamentMetadata";
 
 interface TournamentStatusBadgesProps {
   tournament: Tournament;
 }
 
 export const TournamentStatusBadges = (props: TournamentStatusBadgesProps) => {
+  const utcOffset = useUtcOffset(props.tournament.id);
+
   return (
     <Flex className="justify-start gap-2 w-fit ml-6">
       {props.tournament.tournamentStatus === 'not-started' && (
-        <Badge color='purple'>Upcoming</Badge>
+        <Badge color='purple'>{formatTournamentStatus(props.tournament, utcOffset)}</Badge>
       )}
       {props.tournament.tournamentStatus === 'running' && (
         <Badge icon={StatusOnlineIcon}>Live</Badge>
