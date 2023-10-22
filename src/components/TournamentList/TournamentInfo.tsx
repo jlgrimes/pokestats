ff1a5e87c031b35110ea380ad1f8071377fa5f98
BuildTools <unconfigured@null.spigotmc.org>
2023-10-21 21:47:52 -0500

    refactored tournament card

--- src/components/TournamentList/TournamentInfo.tsx
+++ src/components/TournamentList/TournamentInfo.tsx
@@ -1,11 +1,12 @@
 import { LinkOverlay } from '@chakra-ui/react';
 import NextLink from 'next/link';
-import { Flex, Grid, Subtitle, Title } from '@tremor/react';
+import { Badge, Flex, Grid, Subtitle, Title } from '@tremor/react';
 import { Tournament } from '../../../types/tournament';
 import { formatTournamentDate } from './helpers';
 import { TournamentStatusBadges } from './TournamentStatusBadges';
 import { CountryFlag } from '../Tournament/Home/CountryFlag';
 import { useCountryCode } from '../../hooks/tournamentMetadata';
+import { StatusOnlineIcon } from '@heroicons/react/outline';
 
 interface TournamentInfoProps {
   tournament: Tournament;
@@ -15,23 +16,17 @@ export const TournamentInfo = (props: TournamentInfoProps) => {
   const countryCode = useCountryCode(props.tournament.id);
 
   return (
-      <Grid className='grid-cols-[auto_1fr] gap-4'>
-         {countryCode ? <CountryFlag countryCode={countryCode} size={'sm'} /> : null}
-        <Flex className='w-fit'>
-          <Flex className='flex-col grow items-start'>
-            <LinkOverlay as={NextLink} href={`/tournaments/${props.tournament.id}`}>
-              <div className='flex'>
-                <Title>
-                  {props.tournament.name}
-                </Title>
-                <TournamentStatusBadges tournament={props.tournament} />
-              </div>
-              <Subtitle>
-                {formatTournamentDate(props.tournament)}
-              </Subtitle>
-            </LinkOverlay>
-          </Flex>
-        </Flex>
-      </Grid>
-  );
+    <Flex>
+      <div>
+        <Title>{props.tournament.name}</Title>
+        <div className='flex gap-2'>
+          <Subtitle>{formatTournamentDate(props.tournament)}</Subtitle>
+          {props.tournament.tournamentStatus === 'running' && (
+            <Badge size='xs' icon={StatusOnlineIcon}>Live</Badge>
+          )}
+        </div>
+      </div>
+      {countryCode ? <CountryFlag countryCode={countryCode} size={'sm'} /> : null}
+    </Flex>
+  )
 };
