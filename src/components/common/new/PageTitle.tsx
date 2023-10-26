0c667bda0c3c4cd8d449e9487a25828f25c82cb3
BuildTools <unconfigured@null.spigotmc.org>
2023-10-25 20:50:36 -0500

    made page title slightly smaller

--- src/components/common/new/PageTitle.tsx
+++ src/components/common/new/PageTitle.tsx
@@ -2,5 +2,5 @@ import { Title } from "@tremor/react";
 import { PropsWithChildren } from "react";
 
 export const PageTitle = (props: PropsWithChildren) => (
-  <h1 className="text-3xl font-bold leading-snug text-slate-700 text-center">{props.children}</h1>
+  <h1 className="text-2xl font-bold leading-snug text-slate-700 text-center">{props.children}</h1>
 )
