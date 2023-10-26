b6063840d9e445d7521e1f40352ac391556634f8
BuildTools <unconfigured@null.spigotmc.org>
2023-10-25 20:56:13 -0500

    fixed wonky home page layout

--- src/components/TournamentList/TournamentList.tsx
+++ src/components/TournamentList/TournamentList.tsx
@@ -25,7 +25,7 @@ export const TournamentList = ({
   return (
     <>
     {liveTournaments.map((tournament) => (
-      <div className='flex flex-col py-4 gap-2' key={`tournament-card-${tournament.id}`}>
+      <div className='flex flex-col gap-2' key={`tournament-card-${tournament.id}`}>
         <TournamentCard
           tournament={tournament}
           champion={undefined}
@@ -42,7 +42,7 @@ export const TournamentList = ({
     ))}
     {
       finishedTournaments.slice(0, mostRecent ? 3 : -1).map((tournament) => (
-        <div className='flex flex-col py-4 gap-2' key={`tournament-card-${tournament.id}`}>
+        <div className='flex flex-col gap-2' key={`tournament-card-${tournament.id}`}>
           <TournamentCard
             tournament={tournament}
             champion={champions ? champions.find((standing) => standing.tournament_id === parseInt(tournament.id)) : undefined}
