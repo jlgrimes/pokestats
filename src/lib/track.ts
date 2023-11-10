91547eea7942f91ea1c5044e338f72d8c054172a
BuildTools <unconfigured@null.spigotmc.org>
2023-11-10 17:27:07 -0600

    added environment and pathname

--- src/lib/track.ts
+++ src/lib/track.ts
@@ -1,5 +1,11 @@
 import { track } from "@vercel/analytics/react"
 
 export const trackEvent = (action: string, info?: Record<string, any>) => {
-  track(action, info);
+  const properties: Record<string, any> = {
+    ...info,
+    environment: process.env.NEXT_PUBLIC_ENV,
+    pathname: window?.location?.pathname
+  }
+
+  track(action, properties);
 }
