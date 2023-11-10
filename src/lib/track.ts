410c9cee581bda5f7b445ba2eaa7289bf01a54a5
BuildTools <unconfigured@null.spigotmc.org>
2023-11-10 17:42:10 -0600

    remove env bc i don't need it i guess??

--- src/lib/track.ts
+++ src/lib/track.ts
@@ -3,7 +3,6 @@ import { track } from "@vercel/analytics/react"
 export const trackEvent = (action: string, info?: Record<string, any>) => {
   const properties: Record<string, any> = {
     ...info,
-    environment: process.env.NEXT_PUBLIC_ENV,
     pathname: window?.location?.pathname
   }
 
