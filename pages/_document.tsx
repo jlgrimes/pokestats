a776a1592d68b74dc76d95d7ee10d320bfc62c83
Jared Grimes <jaredlgrimes@hotmail.com>
2023-12-15 12:15:21 -0600

    figuring out what activates ads

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,7 +13,7 @@ export default class Document extends NextDocument {
           src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3066736963130742'
           crossOrigin='anonymous'
         ></script>
-        <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+        {/* <script>(adsbygoogle = window.adsbygoogle || []).push({});</script> */}
         <body>
           {/* 👇 Here's the script */}
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
