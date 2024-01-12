c4ecd870c0b891c211b3155540137950a939d1ce
BuildTools <unconfigured@null.spigotmc.org>
2024-01-11 18:10:53 -0600

    remove ads when you switch pages too

--- src/components/Ad.tsx
+++ src/components/Ad.tsx
@@ -1,6 +1,13 @@
 import { Box } from '@chakra-ui/react';
 import { Adsense } from '@ctrl/react-adsense';
+import { useShouldRemoveAds } from '../hooks/ads';
 
-export const Ad = ({ slot, height }: { slot?: string, height?: string }) => (
-  <Adsense client='ca-pub-3066736963130742' slot={slot ?? '5287940290'} {...(height ? { style: { display: 'block', height, width: '100%' }, format: ''} : {})} />
-);
+export const Ad = ({ slot, height }: { slot?: string, height?: string }) => {
+  const shouldDisableAds = useShouldRemoveAds();
+
+  if (shouldDisableAds) return null;
+
+  return (
+    <Adsense client='ca-pub-3066736963130742' slot={slot ?? '5287940290'} {...(height ? { style: { display: 'block', height, width: '100%' }, format: ''} : {})} />
+  );
+};
