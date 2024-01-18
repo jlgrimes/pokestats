e75405d83442b62af2614958b3bf4598fc5e6f88
BuildTools <unconfigured@null.spigotmc.org>
2024-01-18 17:58:05 -0600

    whoops /

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -36,7 +36,7 @@ export default function App({
           ></script>
         </Head> */}
         <Head>
-          <link rel="manifest" href="/app.webmanifest">
+          <link rel="manifest" href="/app.webmanifest" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
           <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@500&display=swap" rel="stylesheet" />
