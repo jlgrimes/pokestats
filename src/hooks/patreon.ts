e5037e0dab3ce104a0bc52ba9b4c7da72dff0f95
Jared Grimes <jaredlgrimes@hotmail.com>
2024-02-15 12:52:03 -0600

    added patron exception logic

--- src/hooks/patreon.ts
+++ src/hooks/patreon.ts
@@ -1,5 +1,6 @@
 import { useUser } from "@supabase/auth-helpers-react";
 import { useQuery } from "@tanstack/react-query";
+import supabase from "../lib/supabase/client";
 
 const fetchPatrons = async () => {
   const res = await fetch('/api/patreon-pledges');
@@ -15,9 +16,24 @@ export const usePatrons = () => {
   })
 }
 
+const fetchPatronExceptions = async () => {
+  const res = await supabase.from('Patreon Users').select('email');
+
+  return res.data;
+}
+
+export const usePatronExceptions = () => {
+  return useQuery({
+    queryKey: ['pokestats-patron-exceptions'],
+    queryFn: fetchPatronExceptions
+  })
+}
+
 export const useIsUserPatron = () => {
   const user = useUser();
   const { data: patrons } = usePatrons();
+  const { data: additionalPatrons } = usePatronExceptions();
 
-  return patrons?.some((patron: Record<any, any>) => patron.attributes.email === user?.email || patron.attributes.full_name === user?.user_metadata.name);
+  const userIsManuallyAddedToDb = user?.email && additionalPatrons?.some(({ email }) => email === user.email);
+  return userIsManuallyAddedToDb || patrons?.some((patron: Record<any, any>) => patron.attributes.email === user?.email || patron.attributes.full_name === user?.user_metadata.name);
 }
