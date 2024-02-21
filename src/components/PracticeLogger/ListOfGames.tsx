333aab22493775ff71c45f7fb91c5b8d0cfe8884
Jared Grimes <jaredlgrimes@hotmail.com>
2024-02-20 22:15:00 -0600

    changed a little bit

--- src/components/PracticeLogger/ListOfGames.tsx
+++ src/components/PracticeLogger/ListOfGames.tsx
@@ -1,11 +1,14 @@
 import { GameLog, useGameLogs } from "./useGameLogs"
 import { GameModalPreview } from "./GameModalPreview";
+import { Text, Title } from "@tremor/react";
 
 export const ListOfGames = () => {
   const { data: gameLogs } = useGameLogs();
 
   return (
     <div className="flex flex-col gap-2">
+      <Title>My games</Title>
+      {gameLogs?.length === 0 && <Text>Add a game to get started</Text>}
       {gameLogs?.map((game: GameLog) => (
         <GameModalPreview key={game.id} gameLog={game} />
       ))}
