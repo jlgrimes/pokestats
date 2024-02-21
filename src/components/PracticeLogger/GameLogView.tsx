2d30ddb8e11fa123111e59d5ad63a0ce7bb66f3f
Jared Grimes <jaredlgrimes@hotmail.com>
2024-02-20 20:13:47 -0600

    added accordions for game actions

--- src/components/PracticeLogger/GameLogView.tsx
+++ src/components/PracticeLogger/GameLogView.tsx
@@ -1,4 +1,4 @@
-import { List, ListItem, Text } from "@tremor/react";
+import { Accordion, AccordionBody, AccordionHeader, AccordionList, List, ListItem, Text } from "@tremor/react";
 import { GameLog } from "./useGameLogs"
 
 interface GameLogViewProps {
@@ -10,14 +10,20 @@ export const GameLogView = (props: GameLogViewProps) => {
     <List className="p-4">
       {props.gameLog.log.map((action, idx) => (
         <ListItem key={`${props.gameLog.id}-log-action-${idx}`}>
-          <div>
-            <p>{action.message}</p>
-            <div className="pl-4">
-              {action.actionMechanics?.map((mechanic, mechIdx) => (
-                <p className="text-gray-500 text-sm" key={`${props.gameLog.id}-log-action-${idx}-${mechIdx}`}>{mechanic.message}</p>
-              ))}
-            </div>
-          </div>
+          {
+            action.actionMechanics ? (
+              <Accordion className="p-0 [&>button]:p-0 border-none w-full">
+                <AccordionHeader className="text-left">{action.message}</AccordionHeader>
+                <AccordionBody className="pl-4 pb-0">
+                  {action.actionMechanics?.map((mechanic, mechIdx) => (
+                    <p className="text-gray-500 text-sm" key={`${props.gameLog.id}-log-action-${idx}-${mechIdx}`}>{mechanic.message}</p>
+                  ))}
+                </AccordionBody>
+              </Accordion>
+            ) : (
+              <p>{action.message}</p>
+            )
+          }
         </ListItem>
       ))}
     </List>
